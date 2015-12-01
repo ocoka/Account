8a6bca18baa6baee808576221e46d7f9efa35bad
Ivan Sysoev <ivan.sysoev@apm-consult.com>
2015-12-01 22:33:34 +0300

    Acceptable markup and light controller

--- app/main.js
+++ app/main.js
@@ -1,10 +1,6 @@
 /*global angular */
 angular.module("oAccount",["ngAnimate"]).controller("oOutcomes",["$scope",'$rootScope',function($scope,$root){
+  $scope.state={sum:0,stage:'categories',fin:null,category:null};
+  $scope.categories={c1:{name:'Cat1'},c2:{name:'Cat2'},c3:{name:'Cat3'},c4:{name:'Cat4'}};
   $root.loaded=true;
-  this.categories={first:{name:'Cat1'},second:{name:'Cat2'}};
-  this.selected={category:null,sum:null}
-  this.setCategory=function setCategory(catID){
-    this.selected.category=catID;
-    this.selected.sum=0;
-  }
 }]);
